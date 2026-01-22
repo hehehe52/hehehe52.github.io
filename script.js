9c4fad5313da416fe56ca58e62cd689e56e49cff
hehehe52 <122081689+hehehe52@users.noreply.github.com>
2026-01-22 17:19:35 +0800

    gave the grid more vertical and horizontal space

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ for (i = 0; i < acc.length; i++){
         panel.classList.toggle("after");
         if (panel.style.display === "block")
             panel.style.display = "none";
-        else panel.style.display = "block";
+        else panel.style.display = "block"; 
     })
 };
 
