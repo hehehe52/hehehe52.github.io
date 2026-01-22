67cf6f629f701b61df4891860e07116eb55d9a91
hehehe52 <122081689+hehehe52@users.noreply.github.com>
2026-01-22 16:37:28 +0800

    Replaced and labeled checklist with radio buttons.

--- script.js
+++ script.js
@@ -78,8 +78,20 @@ for (i = 0; i < acc.length; i++){
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
         let panel = this.nextElementSibling;
+        panel.classList.toggle("after");
         if (panel.style.display === "block")
             panel.style.display = "none";
         else panel.style.display = "block";
     })
 };
+
+
+
+//test
+/*
+const panel = document.querySelectorAll(".panel");
+panel.addEventListener("click", function() {
+    panel.classList.toggle("active");
+});
+
+*/
