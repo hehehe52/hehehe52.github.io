5b5d66412c93cb27053ea6a148215277483161e4
hehehe52 <122081689+hehehe52@users.noreply.github.com>
2026-01-28 22:19:31 +0800

    Added spreadsheet implementation
    Upon submitting form, task items that have failed inspection and task items added by the user will be added to a google spreadsheet via google app script. Added task items are checked 'No' by default as adding them implies that they are additional misc tasks that have failed inspection.

--- script.js
+++ script.js
@@ -39,11 +39,12 @@ btn.addEventListener("click", function () {
 //button + modal logic using querySelector method
 const modalDialog = document.querySelector("#taskModal");
 const showModal = document.querySelector("#modal-button");
-const closeModal = document.querySelector("#modal-close-button")
-const list = document.querySelector("#list")
-const taskNameInput = document.querySelector("#modal-task-name-input")
+const closeModal = document.querySelector("#modal-close-button");
+const list = document.querySelector("#list");
+const taskNameInput = document.querySelector("#modal-task-name-input");
 
-//open modal on click
+
+//open add new task modal on click
 showModal.addEventListener("click", function() {
     modalDialog.showModal();
     modalDialog.returnValue = "";
@@ -52,10 +53,6 @@ showModal.addEventListener("click", function() {
     // this clears the value before each input attempt into the modal. hacky fix to ensure old DOM data gets cleared
 });
 
-//close modal on click x button
-/*closeModal.addEventListener("close", function() {
-    modalDialog.close();
-}); */
 
 //check if return value of modal is 'save', means user wants to add new item, so we add the entry into the list
 modalDialog.addEventListener("close", function() {
@@ -66,15 +63,15 @@ modalDialog.addEventListener("close", function() {
         console.log("User saved")
 
         
-        const targetList = document.querySelector("#list-entrance-additional"); //target the <ol> element with id=list-entrance-additional
-        const addListNode = document.createElement("li");
+        const targetList = document.querySelector("#list-additional"); //target the <ol> element with id=list-entrance-additional
+        const addListNode = document.createElement("li"); 
         const uniqueID = Date.now();
         
         addListNode.innerHTML = `
                             <span>${text}</span> 
                             <span>
                                 <label for="${uniqueID}yes">Yes</label> <input type="radio" id="${uniqueID}yes" name="${uniqueID}" value="Yes">
-                                <label for="${uniqueID}no">No</label> <input type="radio" id="${uniqueID}no" name="${uniqueID}" value="No">
+                                <label for="${uniqueID}no">No</label> <input type="radio" id="${uniqueID}no" name="${uniqueID}" value="No" checked>
                                 <label for="${uniqueID}NIL">NIL</label> <input type="radio" id="${uniqueID}NIL" name="${uniqueID}" value="NIL">
                             </span>
         `;
@@ -84,7 +81,21 @@ modalDialog.addEventListener("close", function() {
     }
 });
 
+//upon page loading, modal will pop up and require user to enter room number
+let roomNumber = ""
 
+const roomNumberModal = document.querySelector("#room-number-modal")
+const roomNumberInput = document.querySelector("#room-number-input")
+const roomNumberDisplay = document.querySelector("#room-number-display")
+
+window.addEventListener("load", () => {
+    roomNumberModal.showModal();
+}); //change to non arrow function by ctrl f =>
+
+roomNumberModal.addEventListener("close", function() {
+    roomNumber = roomNumberInput.value;
+    roomNumberDisplay.textContent = roomNumberInput.value;
+})
 
 //add accordions for better readability
 const acc = document.getElementsByClassName("accordion");
@@ -101,7 +112,46 @@ for (i = 0; i < acc.length; i++){
     })
 };
 
+//add submit button logic to send data to google forms
+const inspectionForm = document.querySelector("#inspection-form");
 
+inspectionForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const reportData = []; //send this to spreadsheet
+
+    const allItems = document.querySelectorAll("li") //select all <li> elements
+
+    //loop through all items in the checklist, check for any list item with 'no' checked 
+    allItems.forEach(function(item) {
+        const taskName = item.querySelector("span").textContent; //it will always select the span containing the task name as it is the first span
+
+        const taskStatus =  item.querySelector("input[type='radio']:checked"); //grab the value of this task's radio button 
+        
+        if (taskStatus.value === "No") { //if value of the radio button is no, the taskName and taskStatus of the current item in the loop will be added to the reportData array
+            reportData.push({
+                room: roomNumber,
+                task: taskName,
+                status: taskStatus.value
+            });
+        }
+    });
+    sendBatchToGoogle(reportData); //POST request with the array of all the failed items. shld be outside of the for each loop
+});
+
+function sendBatchToGoogle(putReportDataHere) { //putReportDataHere is where reportData is supposed to go when using the sendBatchToGoogle func
+    const URL = "https://script.google.com/macros/s/AKfycbyR_slhptgyCwRUKGsE4PA86uecD9XiGn5lrjCBzbqww_qJ-hHXCPgjc6dgFjdN4hHGKQ/exec"
+
+    fetch(URL, {
+        method: "POST",
+        mode: "no-cors", //deliver the message, ignore the response and errors, if not google will block the req
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(putReportDataHere)
+    })
+
+    .then( function() {
+        alert("Report Submitted Successfully!");
+    })
+}
 
 //test
 /*
