419d141da56d285eef7fdf656408c2ff12e30189
hehehe52 <122081689+hehehe52@users.noreply.github.com>
2026-02-09 00:14:09 +0800

    Cleaned up source code
    Deleted unused code, deleted old comments, added new comments

--- script.js
+++ script.js
@@ -1,41 +1,3 @@
-//addListNode is an element node that is used as a list item
-
-/*when add button is pressed, this function is called and it creates a element node and a text node, adds 
-function getInputValueFunction() {
-    let addListNode = document.createElement("li"); //creates itemnode as a list item
-    
-    //labels
-    let inputValue = document.getElementById("addnewtask-input").value; //getElementByID reads the value of textfield and assigns it to inputValue
-    addListNode.append(inputValue); //assigns the text content of inputValue to itemnode
-
-    //yes checkbox
-    let addYesCheckbox = document.createElement("input"); //creates checkbox element
-    addYesCheckbox.type="checkbox"; //sets element to checkbox type
-    addListNode.append(addYesCheckbox); //appends checkbox to list it
-    
-    //no checkbox
-    let addNoCheckbox = document.createElement("input"); //creates element for nocheckbox
-    addNoCheckbox.type="checkbox" //sets nocheckbox element type to checkbox
-    addListNode.append(addNoCheckbox); //append nocheckbox element to list element
-
-
-    addListNode.onclick = function() { //when the itemnode is clicked, it is removed from the list
-        this.remove(); 
-    }; 
-
-    document.getElementById("list").append(addListNode); //appends the itemnode to the unordered list with the ID "list"
-}; */
-
-//add button + modal logic
-//set listener for button click. on button click unhide modal
-/*const modal = document.getElementById("modal");
-const btn = document.getElementById("modal-button");
-const confirmBtn = document.getElementById("modal-save-button");
-const closeBtn = document.getElementById("modal-close-button"); 
-btn.addEventListener("click", function () {
-    modal.style.display = "block"; 
-})*/
-
 //button + modal logic using querySelector method
 const modalDialog = document.querySelector("#taskModal");
 const showModal = document.querySelector("#modal-button");
@@ -47,26 +9,23 @@ const taskNameInput = document.querySelector("#modal-task-name-input");
 //open add new task modal on click
 showModal.addEventListener("click", function() {
     modalDialog.showModal();
-    modalDialog.returnValue = "";
-    console.log(modalDialog.returnValue)
-    // this line clears the value in returnValue property of modalDialog element. 
-    // this clears the value before each input attempt into the modal. hacky fix to ensure old DOM data gets cleared
+    modalDialog.returnValue = ""; 
+    // this line clears the value in returnValue property of modalDialog element before each input attempt.//
+    //hacky fix to ensure old DOM data gets cleared, if not returnValue will forever be stuck at the first value assigned to it,// 
+    //making the cross and tick buttons not work correctly// 
 });
 
 
 //check if return value of modal is 'save', means user wants to add new item, so we add the entry into the list
 modalDialog.addEventListener("close", function() {
-
-
     if (modalDialog.returnValue === 'save') {
         let text = taskNameInput.value.trim(); //use trim function to get rid of spaces around the text
-        console.log("User saved")
 
-        
         const targetList = document.querySelector("#list-additional"); //target the <ol> element with id=list-entrance-additional
         const addListNode = document.createElement("li"); 
         const uniqueID = Date.now();
-        
+
+//template for the html structure of the new item using template literal, {text} is the task name entered by the user
         addListNode.innerHTML = `
                             <span>${text}</span> 
                             <span>
@@ -88,9 +47,9 @@ const roomNumberModal = document.querySelector("#room-number-modal")
 const roomNumberInput = document.querySelector("#room-number-input")
 const roomNumberDisplay = document.querySelector("#room-number-display")
 
-window.addEventListener("load", () => {
+window.addEventListener("load", function() {
     roomNumberModal.showModal();
-}); //change to non arrow function by ctrl f =>
+});
 
 roomNumberModal.addEventListener("close", function() {
     roomNumber = roomNumberInput.value;
@@ -102,10 +61,10 @@ const acc = document.getElementsByClassName("accordion");
 let i;
 
 for (i = 0; i < acc.length; i++){
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
+    acc[i].addEventListener("click", function() { //adds listener for every <button class=accordion>
+        this.classList.toggle("active"); //adds active to the classList of every <button class=accordion>
+        this.classList.toggle("after");
         let panel = this.nextElementSibling;
-        panel.classList.toggle("after");
         if (panel.style.display === "block")
             panel.style.display = "none";
         else panel.style.display = "block"; 
@@ -150,14 +109,6 @@ function sendBatchToGoogle(putReportDataHere) { //putReportDataHere is where rep
 
     .then( function() {
         alert("Report Submitted Successfully!");
+        location.reload(); 
     })
 }
-
-//test
-/*
-const panel = document.querySelectorAll(".panel");
-panel.addEventListener("click", function() {
-    panel.classList.toggle("active");
-});
-
-*/
